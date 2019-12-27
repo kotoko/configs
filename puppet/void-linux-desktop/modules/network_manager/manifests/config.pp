81f7165c20cdb0c08d741be5e4ec60d3d02adfc3
kotoko <kotoko@users.noreply.github.com>
2019-12-27 20:02:08 +0100

    puppet: Rename polkit files

--- puppet/void-linux-desktop/modules/network_manager/manifests/config.pp
+++ puppet/void-linux-desktop/modules/network_manager/manifests/config.pp
@@ -1,10 +1,10 @@
 class network_manager::config {
 	# Allow users to manage connections.
-	file { '/etc/polkit-1/rules.d/50-org.freedesktop.NetworkManager.rules':
+	file { '/etc/polkit-1/rules.d/51-org.freedesktop.NetworkManager.rules':
 		ensure => 'file',
 		backup => false,
 		owner => 'root',
 		mode => '0644',
-		source => 'puppet:///modules/network_manager/50-org.freedesktop.NetworkManager.rules',
+		source => 'puppet:///modules/network_manager/51-org.freedesktop.NetworkManager.rules',
 	}
 }
--- puppet/void-linux-desktop/modules/shutdown_permission/manifests/polkit.pp
+++ puppet/void-linux-desktop/modules/shutdown_permission/manifests/polkit.pp
@@ -1,9 +1,9 @@
 class shutdown_permission::polkit {
-	file { '/etc/polkit-1/rules.d/51-shutdown.rules':
+	file { '/etc/polkit-1/rules.d/52-shutdown.rules':
 		ensure => 'file',
 		backup => false,
 		owner => 'root',
 		mode => '0644',
-		source => 'puppet:///modules/shutdown_permission/51-shutdown.rules',
+		source => 'puppet:///modules/shutdown_permission/52-shutdown.rules',
 	}
 }
