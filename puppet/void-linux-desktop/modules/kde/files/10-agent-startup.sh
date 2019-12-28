92e3973106c04eaa77b7a00543ca21b0c9f19f5d
kotoko <kotoko@users.noreply.github.com>
2019-12-28 15:43:21 +0100

    puppet: Update kde agents

--- puppet/void-linux-desktop/modules/kde/files/10-agent-startup.sh
+++ puppet/void-linux-desktop/modules/kde/files/10-agent-startup.sh
@@ -4,8 +4,8 @@
 # the environment variables set here are available
 # throughout the session.
 
-if [ -x /usr/bin/gpg-agent ]; then
-  eval "$(/usr/bin/gpg-agent --daemon)"
+if [ -x /usr/bin/gpgconf ]; then
+    /usr/bin/gpgconf --launch gpg-agent >/dev/null 2>&1
 fi
 
 if [ -x /usr/bin/ssh-agent ]; then
