1cee5ba2acd6dbf1dcc6c4f11a94e68767e69746
kotoko <kotoko@users.noreply.github.com>
2020-01-11 14:26:47 +0100

    puppet: Fixes

--- puppet/void-linux-desktop/modules/users/manifests/cache_to_ram.pp
+++ puppet/void-linux-desktop/modules/users/manifests/cache_to_ram.pp
@@ -2,7 +2,7 @@ class users::cache_to_ram {
 	file { '/etc/profile.d/xdg_cache_home.sh':
 		ensure => 'file',
 		backup => false,
-		owner => $user,
+		owner => 'root',
 		mode => '0644',
 		source => 'puppet:///modules/users/xdg_cache_home.sh',
 	}
