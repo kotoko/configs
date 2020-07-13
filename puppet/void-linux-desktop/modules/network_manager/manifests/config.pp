4214d47a3bc8d95cac33a5ef85af45730b1350a2
kotoko <kotoko@users.noreply.github.com>
2020-07-13 11:18:55 +0200

    puppet: Polkit rules for NM are in official package
    https://github.com/void-linux/void-packages/pull/21608/commits/0249a6980a9af2724aa06a530570bc2d2529262e

--- puppet/void-linux-desktop/modules/network_manager/manifests/config.pp
+++ puppet/void-linux-desktop/modules/network_manager/manifests/config.pp
@@ -1,18 +1,6 @@
 class network_manager::config {
-	# Allow users to manage connections.
-	file { '/etc/polkit-1/rules.d/51-org.freedesktop.NetworkManager.rules':
-		ensure => 'file',
-		backup => false,
-		owner => 'polkitd',
-		mode => '0644',
-		source => 'puppet:///modules/network_manager/51-org.freedesktop.NetworkManager.rules',
-	}
-
 	group { 'network':
 		ensure   => 'present',
 		provider => 'groupadd',
 	}
-
-	File['/etc/polkit-1/rules.d'] ->
-	File['/etc/polkit-1/rules.d/51-org.freedesktop.NetworkManager.rules']
 }
