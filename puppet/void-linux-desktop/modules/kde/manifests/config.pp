3723c0252097c5a728e8be617ef6c989551438f5
kotoko <kotoko@users.noreply.github.com>
2021-01-17 19:31:36 +0100

    Remove outdated pam config
    See:
    https://github.com/void-linux/void-packages/commit/e065ac229d78c4a1819098c7bda45bbd717120ba

--- puppet/void-linux-desktop/modules/kde/manifests/config.pp
+++ puppet/void-linux-desktop/modules/kde/manifests/config.pp
@@ -1,9 +1,4 @@
 class kde::config {
-	# Remove 'nox11' (from pam config) from line with 'pam_ck_connector.so'.
-	exec { 'sed_system-login':
-		command => '/sbin/sed -i -r "s/(pam_ck_connector\.so)(.*)nox11(.*)$/\1\2\3/" /etc/pam.d/system-login',
-	}
-
 	$dirs = [
 		'/etc/xdg',
 		'/etc/xdg/plasma-workspace',
