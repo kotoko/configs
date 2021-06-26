5c333efc1ae2ccd5d7b4e587056fc4a709b64520
kotoko <kotoko@users.noreply.github.com>
2021-06-26 18:37:05 +0200

    puppet: Fix repo synchronization

--- puppet/void-linux-desktop/modules/void/manifests/repo_nonfree.pp
+++ puppet/void-linux-desktop/modules/void/manifests/repo_nonfree.pp
@@ -1,5 +1,12 @@
 class void::repo_nonfree {
-  void::package { 'void-repo-nonfree':
-    ensure => 'installed',
-  }
+	void::package { 'void-repo-nonfree':
+		ensure => 'installed',
+	}
+
+	exec { 'repo_nonfree_synchronize':
+		command => '/usr/bin/yes y | /usr/bin/xbps-install --sync',
+	}
+
+	void::package['void-repo-nonfree'] ->
+	exec['repo_nonfree_synchronize']
 }
