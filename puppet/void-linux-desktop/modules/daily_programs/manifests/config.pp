91f957adc5922cd426ab727a032071c780fc561f
kotoko <kotoko@users.noreply.github.com>
2021-07-07 14:23:19 +0200

    puppet: Better touchpad support for firefox

--- puppet/void-linux-desktop/modules/daily_programs/manifests/config.pp
+++ puppet/void-linux-desktop/modules/daily_programs/manifests/config.pp
@@ -12,4 +12,13 @@ class daily_programs::config {
 		ensure   => 'present',
 		provider => 'groupadd',
 	}
+
+	# Better touchpad support for firefox
+	file { '/etc/profile.d/use-xinput2.sh':
+		ensure => 'file',
+		backup => false,
+		owner => 'root',
+		mode => '0644',
+		source => 'puppet:///modules/daily_programs/use-xinput2.sh',
+	}
 }
