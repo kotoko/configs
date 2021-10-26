3462eaed95a58520c91133af20e5986c2966cc7a
kotoko <kotoko@users.noreply.github.com>
2021-10-26 15:03:51 +0200

    puppet: Update tlp configuration to version 1.4

--- puppet/void-linux-desktop/modules/tlp/manifests/config.pp
+++ puppet/void-linux-desktop/modules/tlp/manifests/config.pp
@@ -1,19 +1,11 @@
 class tlp::config {
-	file { '/etc/default':
-		ensure => 'directory',
-		backup => false,
-		owner => 'root',
-		mode => '0755',
-	}
-
-	file { '/etc/default/tlp':
+	file { '/etc/tlp.conf':
 		ensure => 'file',
 		backup => false,
 		owner => 'root',
 		mode => '0644',
-		source => 'puppet:///modules/tlp/tlp',
+		source => 'puppet:///modules/tlp/tlp.conf',
 	}
 
-	File['/etc/default'] ->
-	File['/etc/default/tlp']
+	File['/etc/tlp.conf']
 }
