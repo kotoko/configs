45e52e4a817afe1953a5430d25ae741d71627725
kotoko <kotoko@users.noreply.github.com>
2022-10-26 22:15:52 +0200

    puppet: Fix blurry text on second monitor on intel

--- puppet/void-linux-desktop/modules/intel_gpu/manifests/init.pp
+++ puppet/void-linux-desktop/modules/intel_gpu/manifests/init.pp
@@ -11,4 +11,16 @@ class intel_gpu {
 			ensure => 'installed',
 		}
 	}
+
+	file { '/etc/X11/xorg.conf.d/20-intel.conf':
+		ensure => 'file',
+		backup => false,
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+		source => 'puppet:///modules/intel_gpu/20-intel.conf',
+	}
+
+	File['/etc/X11/xorg.conf.d'] ->
+	File['/etc/X11/xorg.conf.d/20-intel.conf']
 }
--- puppet/void-linux-desktop/modules/xorg/manifests/config.pp
+++ puppet/void-linux-desktop/modules/xorg/manifests/config.pp
@@ -3,4 +3,23 @@ class xorg::config {
 		ensure   => 'present',
 		provider => 'groupadd',
 	}
+
+	file { '/etc/X11':
+		ensure => 'directory',
+		backup => false,
+		owner => 'root',
+		group => 'root',
+		mode => '0755',
+	}
+
+	file { '/etc/X11/xorg.conf.d':
+		ensure => 'directory',
+		backup => false,
+		owner => 'root',
+		group => 'root',
+		mode => '0755',
+	}
+
+	File['/etc/X11'] ->
+	File['/etc/X11/xorg.conf.d']
 }
