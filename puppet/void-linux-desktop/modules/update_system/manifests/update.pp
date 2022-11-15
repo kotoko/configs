38ff4eda948ccaf2e5d1605027c4dda6c209e3f6
kotoko <kotoko@users.noreply.github.com>
2022-11-15 15:16:23 +0100

    puppet: Small improvement

--- puppet/void-linux-desktop/modules/update_system/manifests/update.pp
+++ puppet/void-linux-desktop/modules/update_system/manifests/update.pp
@@ -1,6 +1,6 @@
 define update_system::update {
 	exec { $title:
-		command => '/usr/bin/yes y | /usr/bin/xbps-install --sync --update',
+		command => '/usr/bin/xbps-install --yes --sync --update',
 		creates => "/tmp/puppet_xbps_synced${title}",
 	}
 }
