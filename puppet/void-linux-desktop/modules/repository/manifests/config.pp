8ab259cdb6b68071451fa3fbd6979e6578ef74fd
kotoko <kotoko@users.noreply.github.com>
2022-12-10 17:52:30 +0100

    puppet: Add nonfree repository

--- puppet/void-linux-desktop/modules/repository/manifests/config.pp
+++ puppet/void-linux-desktop/modules/repository/manifests/config.pp
@@ -7,4 +7,13 @@ class repository::config {
 		mode => '0644',
 		source => 'puppet:///modules/repository/00-repository-main.conf',
 	}
+
+	file { '/etc/xbps.d/10-repository-nonfree.conf':
+		ensure => 'file',
+		backup => false,
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+		source => 'puppet:///modules/repository/10-repository-nonfree.conf',
+	}
 }
