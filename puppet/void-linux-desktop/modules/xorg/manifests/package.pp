db0a1cd8e4c2b7e2ded8028b2356425327676844
kotoko <kotoko@users.noreply.github.com>
2023-12-23 16:38:02 +0100

    puppet: Add xf86-input-mtrack and libinput-gestures

--- puppet/void-linux-desktop/modules/xorg/manifests/package.pp
+++ puppet/void-linux-desktop/modules/xorg/manifests/package.pp
@@ -2,4 +2,12 @@ class xorg::package {
 	void::package { 'xorg':
 		ensure => 'installed',
 	}
+
+	void::package { 'xf86-input-mtrack':
+		ensure => 'installed',
+	}
+
+	void::package { 'libinput-gestures':
+		ensure => 'installed',
+	}
 }
