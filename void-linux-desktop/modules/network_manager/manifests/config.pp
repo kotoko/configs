02964b38ab79f5b9f9d65b41f1cb2994c02cab3f
kotoko <kotoko@users.noreply.github.com>
2025-03-15 21:50:56 +0100

    puppet: Correctly copy file 11-pass-ntp-from-dhcp-to-chrony.sh

--- void-linux-desktop/modules/network_manager/manifests/config.pp
+++ void-linux-desktop/modules/network_manager/manifests/config.pp
@@ -13,6 +13,15 @@ class network_manager::config {
 		source => 'puppet:///modules/network_manager/10-chrony.sh',
 	}
 
+	file { '/etc/NetworkManager/dispatcher.d/11-pass-ntp-from-dhcp-to-chrony.sh':
+		ensure => 'file',
+		backup => false,
+		owner => 'root',
+		group => 'root',
+		mode => '0700',
+		source => 'puppet:///modules/network_manager/11-pass-ntp-from-dhcp-to-chrony.sh',
+	}
+
 	file { '/etc/NetworkManager/dispatcher.d/99-wifi-auto-off.sh':
 		ensure => 'file',
 		backup => false,
